25e8fa7ec1ccd5c6c69ef8af4c4dbdb02d4977b9
ash-R2D2 <avinavashish008@gmail.com>
2023-09-24 21:42:53 -0500

    logic change, major fixes

--- gcpCli.py
+++ gcpCli.py
@@ -1,41 +1,37 @@
-import os
+
 from google.cloud import compute_v1
 
 project_id = 'cse5333-lab1'
 zone_name = 'us-central1-a'
+instance_name = 'Ubuntu-VM-lab1'
 
-compute_client = compute_v1.InstancesClient()
-
-instance_name = 'VM-instance1'
-machine_type = f'zones/{zone_name}/machineTypes/e2-micro'
-image_family = 'debian-12'
-image_project = 'debian-cloud'
-
-boot_disk_size = 10
-
-network_interface = 'nic0'
-network_name = f'projects/{project_id}/global/networks/default'
-
-instance_details = {
-    'name':instance_name,
-    'machineType':machine_type,
-    'disks':[{
+new_instance = compute_v1.Instance()
+new_instance.network_interfaces = [{
+        'name':'nic0',
+        'network':f'projects/{project_id}/global/networks/default'
+    }]
+new_instance.name = instance_name
+new_instance.disks = [{
         'boot':True,
         'initializeParams':{
-            'sourceImage':f'projects/{image_project}/global/images/family/{image_family}'
+            'sourceImage':f'projects/debian-cloud/global/images/family/debian-12'
         },
         'autoDelete':True,
-        'bootSizeGb':boot_disk_size
-    }],
-    'networkInterfaces':[{
-        'name':network_interface,
-        'network':network_name
+        'bootSizeGb':10
     }]
-}
+new_instance.machine_type = f'zones/{zone_name}/machineTypes/e2-micro'
 
-request = compute_client.instances().insert(project=project_id,
-                                  zone=zone_name,
-                                  body=instance_details
-                                  )
-response = request.execute()
-print("instance created...")
+request = compute_v1.InsertInstanceRequest()
+request.zone = zone_name
+request.project = project_id
+request.instance_resource = new_instance
+
+print(f"Creating the {instance_name} instance in {zone_name}...")
+
+instance_client = compute_v1.InstancesClient()
+operation = instance_client.insert(request=request)
+
+#wait_for_extended_operation(operation, "instance creation")
+
+print(f"Instance {instance_name} created.")
+print(instance_client.get(project=project_id, zone=zone_name, instance=instance_name))
